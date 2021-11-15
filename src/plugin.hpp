c7bdf8be9bba60b01a673ba063de21b8fdd0ccd1
danieldavies99 <danieldavies99@gmail.com>
2021-11-15 19:55:29 +0000

    initial commit

---
+++ src/plugin.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <rack.hpp>
+
+using namespace rack;
+
+// Declare the Plugin, defined in plugin.cpp
+extern Plugin* pluginInstance;
+
+// Declare each Model, defined in each module source file
+extern Model* modelMyModule;
+extern Model* modelSeqLights8Module;
+
+/************************** KNOBS **************************/
+
+struct RedKnob : RoundKnob {
+    RedKnob() {
+        setSvg(APP->window->loadSvg(asset::plugin(pluginInstance, "res/RedKnob.svg")));
+    }
+};
+
+struct SteppedRedKnob : RoundKnob {
+    SteppedRedKnob() {
+        setSvg(APP->window->loadSvg(asset::plugin(pluginInstance, "res/RedKnob.svg")));
+        snap = true;
+    }
+};
