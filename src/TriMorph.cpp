ce641889aaa76a5c0da4388375087dc4eaecaa39
danieldavies99 <danieldavies99@gmail.com>
2022-03-28 21:51:47 +0100

    wired up pwm and shift modulation

--- src/TriMorph.cpp
+++ src/TriMorph.cpp
@@ -32,24 +32,23 @@ struct TriMorph : Module {
 
 	TriMorph() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
-		configParam(KNOB_SHIFT_PARAM, -0.5f, 0.5f, 0.f, "");
-		configParam(KNOB_COARSE_PARAM, -4.f, 4.f, 0.f, "");
-		configParam(KNOB_PULSE_WIDTH_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(KNOB_PULSE_WIDTH_MODULATION_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(KNOB_FREQUENCY_MODULATION_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(KNOB_PORTAMENTO_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(KNOB_SHIFT_MODULATION_PARAM, 0.f, 1.f, 0.f, "");
-		configInput(PITCH_IN_INPUT, "");
-		configInput(SHIFT_IN_INPUT, "");
-		configInput(PULSE_WIDTH_MODULATION_IN_INPUT, "");
-		configInput(FREQUENCY_MODULATION_IN_INPUT, "");
-		configOutput(SQUARE_OUT_OUTPUT, "");
-		configOutput(SIN_OUT_OUTPUT, "");
-		configOutput(TRIANGLE_OUT_OUTPUT, "");
+		configParam(KNOB_SHIFT_PARAM, -0.5f, 0.5f, 0.f, "Bend");
+		configParam(KNOB_COARSE_PARAM, -4.f, 4.f, 0.f, "Pitch");
+		configParam(KNOB_PULSE_WIDTH_PARAM, -1.f, 1.f, 0.0f, "Pulse width");
+		configParam(KNOB_PULSE_WIDTH_MODULATION_PARAM, -1.f, 1.f, 0.f, "Pulse width modulation");
+		configParam(KNOB_FREQUENCY_MODULATION_PARAM, -1.f, 1.f, 0.f, "Frequency modulation");
+		configParam(KNOB_SHIFT_MODULATION_PARAM, -1.f, 1.f, 0.f, "Bend modulation");
+		configParam(KNOB_PORTAMENTO_PARAM, 0.f, 1.f, 0.f, "Portamento time");
+		configInput(PITCH_IN_INPUT, "Pitch v/oct");
+		configInput(SHIFT_IN_INPUT, "Bend modulation");
+		configInput(PULSE_WIDTH_MODULATION_IN_INPUT, "Pulse width modulation");
+		configInput(FREQUENCY_MODULATION_IN_INPUT, "Fequency modulation");
+		configOutput(SQUARE_OUT_OUTPUT, "Square out");
+		configOutput(SIN_OUT_OUTPUT, "Sine out");
+		configOutput(TRIANGLE_OUT_OUTPUT, "Tri out");
 	}
 
 	float phase = 0.f;
-	float blinkPhase = 0.f;
 
 	void process(const ProcessArgs& args) override {
 		// Compute the frequency from the pitch parameter and input
@@ -67,14 +66,8 @@ struct TriMorph : Module {
 		// phase oscilates between 0.5 and -0.5;
 		float tri;
 		float shift = params[KNOB_SHIFT_PARAM].getValue();
-
-		// regular triangle:
-		// if(phase < 0) {
-		// 	tri = 4.f * (phase);
-		// }  else {
-		// 	tri = 4.f * -(phase);
-		// }
-		// tri += 1;
+		shift += (inputs[SHIFT_IN_INPUT].getVoltage() / 20) * params[KNOB_SHIFT_MODULATION_PARAM].getValue();
+		shift = clamp(shift, -0.5, 0.5);
 
 		if(phase < shift) {
 			tri = 4.f * phase  - (4 * (shift * ((phase + 0.5) / abs(shift - -0.5 ))) );  // -2 -> 0
@@ -97,7 +90,10 @@ struct TriMorph : Module {
 		outputs[SIN_OUT_OUTPUT].setVoltage(5.f * sine);
 
 		// Compute the square output
-		float pulseWidth = 0;
+		float pulseWidth = params[KNOB_PULSE_WIDTH_PARAM].getValue();
+		pulseWidth += (inputs[PULSE_WIDTH_MODULATION_IN_INPUT].getVoltage() / 10) * params[KNOB_PULSE_WIDTH_MODULATION_PARAM].getValue();
+		pulseWidth = clamp(pulseWidth, -0.99f, 0.99f);
+
 		float square = tri > pulseWidth ? 1 : -1;
 		// sine /= tri;
 		// Audio signals are typically +/-5V
@@ -118,23 +114,23 @@ struct TriMorphWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
 		addParam(createParamCentered<RedKnob>(mm2px(Vec(33.026, 30.329)), module, TriMorph::KNOB_COARSE_PARAM));
-		addParam(createParamCentered<RedKnob>(mm2px(Vec(18.002, 39.713)), module, TriMorph::KNOB_SHIFT_PARAM));
+		addParam(createParamCentered<RedKnob>(mm2px(Vec(18.002, 39.713)), module, TriMorph::KNOB_PULSE_WIDTH_PARAM));
 		addParam(createParamCentered<RedKnob>(mm2px(Vec(47.895, 39.713)), module, TriMorph::KNOB_PORTAMENTO_PARAM));
-		addParam(createParamCentered<RedKnob>(mm2px(Vec(33.026, 64.478)), module, TriMorph::KNOB_PULSE_WIDTH_PARAM));
-		addParam(createParamCentered<RedKnob>(mm2px(Vec(13.709, 81.638)), module, TriMorph::KNOB_SHIFT_MODULATION_PARAM));
-		addParam(createParamCentered<RedKnob>(mm2px(Vec(33.02, 81.638)), module, TriMorph::KNOB_PULSE_WIDTH_MODULATION_PARAM));
+		addParam(createParamCentered<RedKnob>(mm2px(Vec(33.026, 64.478)), module, TriMorph::KNOB_SHIFT_PARAM));
+		addParam(createParamCentered<RedKnob>(mm2px(Vec(13.709, 81.638)), module, TriMorph::KNOB_PULSE_WIDTH_MODULATION_PARAM));
+		addParam(createParamCentered<RedKnob>(mm2px(Vec(33.02, 81.638)), module, TriMorph::KNOB_SHIFT_MODULATION_PARAM)); 
 		addParam(createParamCentered<RedKnob>(mm2px(Vec(52.331, 81.638)), module, TriMorph::KNOB_FREQUENCY_MODULATION_PARAM));
 
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(33.02, 47.518)), module, TriMorph::PITCH_IN_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(13.709, 93.148)), module, TriMorph::SHIFT_IN_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(33.02, 93.148)), module, TriMorph::PULSE_WIDTH_MODULATION_IN_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(13.709, 93.148)), module, TriMorph::PULSE_WIDTH_MODULATION_IN_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(33.02, 93.148)), module, TriMorph::SHIFT_IN_INPUT));
 		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(52.331, 93.148)), module, TriMorph::FREQUENCY_MODULATION_IN_INPUT));
 
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(13.709, 110.457)), module, TriMorph::SQUARE_OUT_OUTPUT));
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(33.02, 110.457)), module, TriMorph::SIN_OUT_OUTPUT));
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(52.331, 110.457)), module, TriMorph::TRIANGLE_OUT_OUTPUT));
 	}
-};
+}; 
 
 
 Model* modelTriMorph = createModel<TriMorph, TriMorphWidget>("TriMorph");
