101084dcdc03de1244e0833839b5aa86bd2606f3
danieldavies99 <danieldavies99@gmail.com>
2025-07-13 22:40:48 +0100

    added patterns and pattern builder script

--- src/Knot.cpp
+++ src/Knot.cpp
@@ -1,12 +1,15 @@
 #include "plugin.hpp"
 #include <algorithm>
 
-struct Knot : Module {
-	enum ParamId {
+struct Knot : Module
+{
+	enum ParamId
+	{
 		KNOB_PATTERN_SELECT_PARAM,
 		PARAMS_LEN
 	};
-	enum InputId {
+	enum InputId
+	{
 		INPUT_PATTERN_RESET_INPUT,
 		INPUT_PATTERN_STEP_INPUT,
 		INPUT_CONTROL_1_2_INPUT,
@@ -27,18 +30,21 @@ struct Knot : Module {
 		INPUT_IN_4_INPUT,
 		INPUTS_LEN
 	};
-	enum OutputId {
+	enum OutputId
+	{
 		OUTPUT_OUT_1_OUTPUT,
 		OUTPUT_OUT_2_OUTPUT,
 		OUTPUT_OUT_3_OUTPUT,
 		OUTPUT_OUT_4_OUTPUT,
 		OUTPUTS_LEN
 	};
-	enum LightId {
+	enum LightId
+	{
 		LIGHTS_LEN
 	};
 
-	Knot() {
+	Knot()
+	{
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
 		configParam(KNOB_PATTERN_SELECT_PARAM, 0.f, 128.f, 0.f, "Pattern Select");
 
@@ -72,7 +78,6 @@ struct Knot : Module {
 		configOutput(OUTPUT_OUT_4_OUTPUT, "");
 	}
 
-	
 	bool isInput1Connected = true;
 	bool isInput2Connected = true;
 	bool isInput3Connected = true;
@@ -83,6 +88,7 @@ struct Knot : Module {
 	int output3Index = 2;
 	int output4Index = 3;
 
+	// manual control gate detectors
 	GateDetector shiftUpDetector;
 	GateDetector shiftDownDetector;
 	GateDetector resetDetector;
@@ -96,6 +102,10 @@ struct Knot : Module {
 	GateDetector undoDetector;
 	GateDetector redoDetector;
 
+	// preset patterns gate detectors
+	GateDetector patternStepDetector;
+	GateDetector patternResetDetector;
+
 	int selectedPattern = 0;
 	bool presetPatternsDisabled = true;
 
@@ -105,91 +115,244 @@ struct Knot : Module {
 	std::deque<KnotUndoState> undoStack;
 	std::deque<KnotUndoState> redoStack;
 
-	
+	// pattern dictionary. might be nice to store this in json or something later
+	KnotPattern patterns[129] = {
+		KnotPattern(""), // empty pattern 0 because pattern zero is unused
+		KnotPattern("Ss"),
+		KnotPattern("XU"),
+		KnotPattern("XRU"),
+		KnotPattern("5RU"),
+		KnotPattern("5U"),
+		KnotPattern("7U"),
+		KnotPattern("Uu6"),
+		KnotPattern("4RU"),
+		KnotPattern("X4SU"),
+		KnotPattern("78U"),
+		KnotPattern("8U"),
+		KnotPattern("3X7U"),
+		KnotPattern("79"),
+		KnotPattern("72"),
+		KnotPattern("78"),
+		KnotPattern("857"),
+		KnotPattern("956"),
+		KnotPattern("35"),
+		KnotPattern("4716"),
+		KnotPattern("945"),
+		KnotPattern("93"),
+		KnotPattern("317"),
+		KnotPattern("61"),
+		KnotPattern("87"),
+		KnotPattern("648"),
+		KnotPattern("73"),
+		KnotPattern("37"),
+		KnotPattern("9542"),
+		KnotPattern("49"),
+		KnotPattern("18"),
+		KnotPattern("142"),
+		KnotPattern("94"),
+		KnotPattern("45"),
+		KnotPattern("672"),
+		KnotPattern("157"),
+		KnotPattern("34"),
+		KnotPattern("421"),
+		KnotPattern("48"),
+		KnotPattern("519"),
+		KnotPattern("9286"),
+		KnotPattern("462"),
+		KnotPattern("7s"),
+		KnotPattern("7S"),
+		KnotPattern("7s7S"),
+		KnotPattern("8SR"),
+		KnotPattern("9s7"),
+		KnotPattern("4S5"),
+		KnotPattern("5s2"),
+		KnotPattern("6S1"),
+		KnotPattern("1s3"),
+		KnotPattern("5s5"),
+		KnotPattern("8s"),
+		KnotPattern("7sS"),
+		KnotPattern("5S8"),
+		KnotPattern("5S4"),
+		KnotPattern("2s1"),
+		KnotPattern("9S9"),
+		KnotPattern("4RU"),
+		KnotPattern("R51"),
+		KnotPattern("R79"),
+		KnotPattern("RS1"),
+		KnotPattern("R8"),
+		KnotPattern("R3U"),
+		KnotPattern("R5u"),
+		KnotPattern("RU5"),
+		KnotPattern("R6u"),
+		KnotPattern("R1S"),
+		KnotPattern("R7u"),
+		KnotPattern("R9s"),
+		KnotPattern("R86"),
+		KnotPattern("567"),
+		KnotPattern("5s2R"),
+		KnotPattern("5252"),
+		KnotPattern("1234"),
+		KnotPattern("sS13"),
+		KnotPattern("65sS"),
+		KnotPattern("sSR"),
+		KnotPattern("1542"),
+		KnotPattern("98s"),
+		KnotPattern("4sS"),
+		KnotPattern("21R"),
+		KnotPattern("3sS"),
+		KnotPattern("765"),
+		KnotPattern("163"),
+		KnotPattern("58s"),
+		KnotPattern("41S"),
+		KnotPattern("X5R"),
+		KnotPattern("X58"),
+		KnotPattern("X4"),
+		KnotPattern("X67"),
+		KnotPattern("X5U"),
+		KnotPattern("Xs"),
+		KnotPattern("XS"),
+		KnotPattern("XR5"),
+		KnotPattern("XUu"),
+		KnotPattern("X6s"),
+		KnotPattern("X4u"),
+		KnotPattern("X8S"),
+		KnotPattern("X54"),
+		KnotPattern("X13"),
+		KnotPattern("X8u"),
+		KnotPattern("RXU"),
+		KnotPattern("XXXUUUuuuUUUuuu"),
+		KnotPattern("XRXUUUuuuUUUuuu"),
+		KnotPattern("XUuUu"),
+		KnotPattern("XsssSSS"),
+		KnotPattern("X1313"),
+		KnotPattern("XR7"),
+		KnotPattern("XR67"),
+		KnotPattern("XRXUUUuuuUUUuuu"),
+		KnotPattern("XXUU"),
+		KnotPattern("X56UUU"),
+		KnotPattern("XsSUUU"),
+		KnotPattern("XssUUU"),
+		KnotPattern("XSSUUU"),
+		KnotPattern("XSSSUUUUuuuu"),
+		KnotPattern("XSSSUUUUX"),
+		KnotPattern("XXUu"),
+		KnotPattern("X6611"),
+		KnotPattern("X7788"),
+		KnotPattern("X7788XUUuu"),
+		KnotPattern("X6611UUuu"),
+		KnotPattern("7XUXUuU7"),
+		KnotPattern("7XUXUuU8"),
+		KnotPattern("13XUXUuU13"),
+		KnotPattern("XRUUuu"),
+		KnotPattern("XRUUuuUUuuUUuu"),
+		KnotPattern("X8UUuuUUuuUUuu")
+	};
 
-	void addCurrentStateToUndoStack() {
-		if (undoStack.size() == MAX_STACK_SIZE) {
+	void addCurrentStateToUndoStack()
+	{
+		if (undoStack.size() == MAX_STACK_SIZE)
+		{
 			undoStack.pop_front();
 		}
 		undoStack.push_back(KnotUndoState(output1Index, output2Index, output3Index, output4Index));
 		redoStack.clear();
 	}
 
-	float getInputVoltage(int inputIndex) {
-		if (inputIndex == 0) {
+	float getInputVoltage(int inputIndex)
+	{
+		if (inputIndex == 0)
+		{
 			return inputs[INPUT_IN_1_INPUT].getVoltage();
 		}
-		if (inputIndex == 1) {
+		if (inputIndex == 1)
+		{
 			return inputs[INPUT_IN_2_INPUT].getVoltage();
 		}
-		if (inputIndex == 2) {
+		if (inputIndex == 2)
+		{
 			return inputs[INPUT_IN_3_INPUT].getVoltage();
 		}
-		if (inputIndex == 3) {
+		if (inputIndex == 3)
+		{
 			return inputs[INPUT_IN_4_INPUT].getVoltage();
 		}
 		return 0.f;
 	}
 
-
-	int getInputId(int inputIndex) {
-		if (inputIndex == 0) {
+	int getInputId(int inputIndex)
+	{
+		if (inputIndex == 0)
+		{
 			return INPUT_IN_1_INPUT;
 		}
-		if (inputIndex == 1) {
+		if (inputIndex == 1)
+		{
 			return INPUT_IN_2_INPUT;
 		}
-		if (inputIndex == 2) {
+		if (inputIndex == 2)
+		{
 			return INPUT_IN_3_INPUT;
 		}
-		if (inputIndex == 3) {
+		if (inputIndex == 3)
+		{
 			return INPUT_IN_4_INPUT;
 		}
 		return -1;
 	}
 
 	// handle control inputs:
-	void handleShiftDown() {
+	void handleShiftDown()
+	{
 		addCurrentStateToUndoStack();
 		output1Index += 1;
 		output2Index += 1;
 		output3Index += 1;
 		output4Index += 1;
-		if (output1Index > 3) {
+		if (output1Index > 3)
+		{
 			output1Index = 0;
 		}
-		if (output2Index > 3) {
+		if (output2Index > 3)
+		{
 			output2Index = 0;
 		}
-		if (output3Index > 3) {
+		if (output3Index > 3)
+		{
 			output3Index = 0;
 		}
-		if (output4Index > 3) {
+		if (output4Index > 3)
+		{
 			output4Index = 0;
 		}
 	}
 
-	void handleShiftUp() {
+	void handleShiftUp()
+	{
 		addCurrentStateToUndoStack();
 		output1Index -= 1;
 		output2Index -= 1;
 		output3Index -= 1;
 		output4Index -= 1;
-		if (output1Index < 0) {
+		if (output1Index < 0)
+		{
 			output1Index = 3;
 		}
-		if (output2Index < 0) {
+		if (output2Index < 0)
+		{
 			output2Index = 3;
 		}
-		if (output3Index < 0) {
+		if (output3Index < 0)
+		{
 			output3Index = 3;
 		}
-		if (output4Index < 0) {
+		if (output4Index < 0)
+		{
 			output4Index = 3;
 		}
 	}
 
-	void handleReset() {
+	void handleReset()
+	{
 		addCurrentStateToUndoStack();
 		output1Index = 0;
 		output2Index = 1;
@@ -197,14 +360,16 @@ struct Knot : Module {
 		output4Index = 3;
 	}
 
-	void handleSwap(int *outputIndex1, int *outputIndex2) {
+	void handleSwap(int *outputIndex1, int *outputIndex2)
+	{
 		addCurrentStateToUndoStack();
 		std::swap(*outputIndex1, *outputIndex2);
 	}
 
-	void handleRandom(bool shouldAddToUndoStack = true) {
+	void handleRandom(bool shouldAddToUndoStack = true)
+	{
 		addCurrentStateToUndoStack();
-		int indexes[4] = { 0, 1, 2, 3 };
+		int indexes[4] = {0, 1, 2, 3};
 		std::random_shuffle(std::begin(indexes), std::end(indexes));
 		output1Index = indexes[0];
 		output2Index = indexes[1];
@@ -212,8 +377,10 @@ struct Knot : Module {
 		output4Index = indexes[3];
 	}
 
-	void handleUndo() {
-		if (undoStack.empty()) return;
+	void handleUndo()
+	{
+		if (undoStack.empty())
+			return;
 
 		KnotUndoState undoState = undoStack.back();
 		undoStack.pop_back();
@@ -225,8 +392,10 @@ struct Knot : Module {
 		output4Index = undoState.output4Index;
 	}
 
-	void handleRedo() {
-		if (redoStack.empty()) return;
+	void handleRedo()
+	{
+		if (redoStack.empty())
+			return;
 
 		KnotUndoState undoState = redoStack.back();
 		redoStack.pop_back();
@@ -239,87 +408,178 @@ struct Knot : Module {
 	}
 	// end handle control inputs
 
-	void process(const ProcessArgs& args) override {
+	void handlePatternStep(KnotControl control)
+	{
+		switch (control)
+		{
+		case ShiftDown:
+			handleShiftDown();
+			break;
+		case ShiftUp:
+			handleShiftUp();
+			break;
+		case Reset:
+			handleReset();
+			break;
+		case Swap12:
+			handleSwap(&output1Index, &output2Index);
+			break;
+		case Swap34:
+			handleSwap(&output3Index, &output4Index);
+			break;
+		case Swap23:
+			handleSwap(&output2Index, &output3Index);
+			break;
+		case Swap14:
+			handleSwap(&output1Index, &output4Index);
+			break;
+		case Swap13:
+			handleSwap(&output1Index, &output3Index);
+			break;
+		case Swap24:
+			handleSwap(&output2Index, &output4Index);
+			break;
+		case Swap1234:
+			handleSwap(&output1Index, &output2Index);
+			handleSwap(&output3Index, &output4Index);
+			break;
+		case Swap1423:
+			handleSwap(&output1Index, &output4Index);
+			handleSwap(&output2Index, &output3Index);
+			break;
+		case Swap1324:
+			handleSwap(&output1Index, &output3Index);
+			handleSwap(&output2Index, &output4Index);
+			break;
+		case Random:
+			handleRandom();
+			break;
+		case Undo:
+			handleUndo();
+			break;
+		case Redo:
+			handleRedo();
+			break;
+		}
+	}
+
+	void process(const ProcessArgs &args) override
+	{
 
 		isInput1Connected = false;
 		isInput2Connected = false;
 		isInput3Connected = false;
 		isInput4Connected = false;
-		if(inputs[INPUT_IN_1_INPUT].isConnected()) {
+		if (inputs[INPUT_IN_1_INPUT].isConnected())
+		{
 			isInput1Connected = true;
 		}
-		if(inputs[INPUT_IN_2_INPUT].isConnected()) {
+		if (inputs[INPUT_IN_2_INPUT].isConnected())
+		{
 			isInput2Connected = true;
 		}
-		if(inputs[INPUT_IN_3_INPUT].isConnected()) {
+		if (inputs[INPUT_IN_3_INPUT].isConnected())
+		{
 			isInput3Connected = true;
 		}
-		if(inputs[INPUT_IN_4_INPUT].isConnected()) {
+		if (inputs[INPUT_IN_4_INPUT].isConnected())
+		{
 			isInput4Connected = true;
 		}
 
-		if (inputs[INPUT_CONTROL_SHIFT_DOWN_INPUT].isConnected() &&shiftDownDetector.check(inputs[INPUT_CONTROL_SHIFT_DOWN_INPUT].getVoltage())) {
+		if (inputs[INPUT_CONTROL_SHIFT_DOWN_INPUT].isConnected() && shiftDownDetector.check(inputs[INPUT_CONTROL_SHIFT_DOWN_INPUT].getVoltage()))
+		{
 			handleShiftDown();
 		}
 
-		if (inputs[INPUT_CONTROL_SHIFT_UP_INPUT].isConnected() && shiftUpDetector.check(inputs[INPUT_CONTROL_SHIFT_UP_INPUT].getVoltage())) {
+		if (inputs[INPUT_CONTROL_SHIFT_UP_INPUT].isConnected() && shiftUpDetector.check(inputs[INPUT_CONTROL_SHIFT_UP_INPUT].getVoltage()))
+		{
 			handleShiftUp();
 		}
 
-		if (inputs[INPUT_CONTROL_RESET_INPUT].isConnected() && resetDetector.check(inputs[INPUT_CONTROL_RESET_INPUT].getVoltage())) {
+		if (inputs[INPUT_CONTROL_RESET_INPUT].isConnected() && resetDetector.check(inputs[INPUT_CONTROL_RESET_INPUT].getVoltage()))
+		{
 			handleReset();
 		}
 
 		// handle 1-2 switch
-		if (inputs[INPUT_CONTROL_1_2_INPUT].isConnected() && swap12Detector.check(inputs[INPUT_CONTROL_1_2_INPUT].getVoltage())) {
+		if (inputs[INPUT_CONTROL_1_2_INPUT].isConnected() && swap12Detector.check(inputs[INPUT_CONTROL_1_2_INPUT].getVoltage()))
+		{
 			handleSwap(&output1Index, &output2Index);
 		}
 
 		// handle 3-4 switch
-		if (inputs[INPUT_CONTROL_3_4_INPUT].isConnected() && swap34Detector.check(inputs[INPUT_CONTROL_3_4_INPUT].getVoltage())) {
+		if (inputs[INPUT_CONTROL_3_4_INPUT].isConnected() && swap34Detector.check(inputs[INPUT_CONTROL_3_4_INPUT].getVoltage()))
+		{
 			handleSwap(&output3Index, &output4Index);
 		}
 
 		// handle 2-3 switch
-		if (inputs[INPUT_CONTROL_2_3_INPUT].isConnected() && swap23Detector.check(inputs[INPUT_CONTROL_2_3_INPUT].getVoltage())) {
+		if (inputs[INPUT_CONTROL_2_3_INPUT].isConnected() && swap23Detector.check(inputs[INPUT_CONTROL_2_3_INPUT].getVoltage()))
+		{
 			handleSwap(&output2Index, &output3Index);
 		}
 
 		// handle 1-4 switch
-		if (inputs[INPUT_CONTROL_1_4_INPUT].isConnected() && swap14Detector.check(inputs[INPUT_CONTROL_1_4_INPUT].getVoltage())) {
+		if (inputs[INPUT_CONTROL_1_4_INPUT].isConnected() && swap14Detector.check(inputs[INPUT_CONTROL_1_4_INPUT].getVoltage()))
+		{
 			handleSwap(&output1Index, &output4Index);
 		}
 
 		// handle 1-3 switch
-		if (inputs[INPUT_CONTROL_1_3_INPUT].isConnected() && swap13Detector.check(inputs[INPUT_CONTROL_1_3_INPUT].getVoltage())) {
+		if (inputs[INPUT_CONTROL_1_3_INPUT].isConnected() && swap13Detector.check(inputs[INPUT_CONTROL_1_3_INPUT].getVoltage()))
+		{
 			handleSwap(&output1Index, &output3Index);
 		}
 
 		// handle 2-4 switch
-		if (inputs[INPUT_CONTROL_2_4_INPUT].isConnected() && swap24Detector.check(inputs[INPUT_CONTROL_2_4_INPUT].getVoltage())) {
+		if (inputs[INPUT_CONTROL_2_4_INPUT].isConnected() && swap24Detector.check(inputs[INPUT_CONTROL_2_4_INPUT].getVoltage()))
+		{
 			handleSwap(&output2Index, &output4Index);
 		}
 
 		// handle random
-		if (inputs[INPUT_CONTROL_RANDOM_INPUT].isConnected() && randomDetector.check(inputs[INPUT_CONTROL_RANDOM_INPUT].getVoltage())) {
+		if (inputs[INPUT_CONTROL_RANDOM_INPUT].isConnected() && randomDetector.check(inputs[INPUT_CONTROL_RANDOM_INPUT].getVoltage()))
+		{
 			handleRandom();
 		}
 
 		// handle undo
-		if (inputs[INPUT_CONTROL_UNDO_INPUT].isConnected() && undoDetector.check(inputs[INPUT_CONTROL_UNDO_INPUT].getVoltage())) {
+		if (inputs[INPUT_CONTROL_UNDO_INPUT].isConnected() && undoDetector.check(inputs[INPUT_CONTROL_UNDO_INPUT].getVoltage()))
+		{
 			handleUndo();
 		}
 
 		// handle redo
-		if (inputs[INPUT_CONTROL_REDO_INPUT].isConnected() && redoDetector.check(inputs[INPUT_CONTROL_REDO_INPUT].getVoltage())) {
+		if (inputs[INPUT_CONTROL_REDO_INPUT].isConnected() && redoDetector.check(inputs[INPUT_CONTROL_REDO_INPUT].getVoltage()))
+		{
 			handleRedo();
 		}
 
+		// handle preset patterns
 		selectedPattern = params[KNOB_PATTERN_SELECT_PARAM].getValue();
-		if (selectedPattern == 0) {
+		if (selectedPattern == 0)
+		{
 			presetPatternsDisabled = true;
-		} else {
+		}
+		else
+		{
 			presetPatternsDisabled = false;
+		}
+
+		if (!presetPatternsDisabled)
+		{
+			// handle step
+			if (inputs[INPUT_PATTERN_STEP_INPUT].isConnected() && patternStepDetector.check(inputs[INPUT_PATTERN_STEP_INPUT].getVoltage()))
+			{	
+				handlePatternStep(patterns[selectedPattern].getNextControl());
+			}
+			// handle reset
+			if (inputs[INPUT_PATTERN_RESET_INPUT].isConnected() && patternResetDetector.check(inputs[INPUT_PATTERN_RESET_INPUT].getVoltage()))
+			{
+				patterns[selectedPattern].reset();
+				handleReset();
+			}
 		}
 
 		// map inputs to outputs
@@ -328,24 +588,29 @@ struct Knot : Module {
 		int out3in = getInputId(output3Index);
 		int out4in = getInputId(output4Index);
 
-		if (inputs[out1in].isConnected()) {
+		if (inputs[out1in].isConnected())
+		{
 			outputs[OUTPUT_OUT_1_OUTPUT].setVoltage(inputs[out1in].getVoltage());
 		}
-		if (inputs[out2in].isConnected()) {
+		if (inputs[out2in].isConnected())
+		{
 			outputs[OUTPUT_OUT_2_OUTPUT].setVoltage(inputs[out2in].getVoltage());
 		}
-		if (inputs[out3in].isConnected()) {
+		if (inputs[out3in].isConnected())
+		{
 			outputs[OUTPUT_OUT_3_OUTPUT].setVoltage(inputs[out3in].getVoltage());
 		}
-		if (inputs[out4in].isConnected()) {
+		if (inputs[out4in].isConnected())
+		{
 			outputs[OUTPUT_OUT_4_OUTPUT].setVoltage(inputs[out4in].getVoltage());
 		}
 	}
 };
 
-
-struct KnotWidget : ModuleWidget {
-	KnotWidget(Knot* module) {
+struct KnotWidget : ModuleWidget
+{
+	KnotWidget(Knot *module)
+	{
 		setModule(module);
 		setPanel(createPanel(asset::plugin(pluginInstance, "res/Knot.svg")));
 
@@ -380,8 +645,9 @@ struct KnotWidget : ModuleWidget {
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(140.366, 80.635)), module, Knot::OUTPUT_OUT_3_OUTPUT));
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(140.366, 101.467)), module, Knot::OUTPUT_OUT_4_OUTPUT));
 
-		if(module) {
-			LineDisplay* lineDisplay = createWidget<LineDisplay>(mm2px(Vec(75.696, 30.732)));
+		if (module)
+		{
+			LineDisplay *lineDisplay = createWidget<LineDisplay>(mm2px(Vec(75.696, 30.732)));
 			lineDisplay->initialize();
 			lineDisplay->in1connected = &module->isInput1Connected;
 			lineDisplay->in2connected = &module->isInput2Connected;
@@ -403,5 +669,4 @@ struct KnotWidget : ModuleWidget {
 	}
 };
 
-
-Model* modelKnot = createModel<Knot, KnotWidget>("Knot");
+Model *modelKnot = createModel<Knot, KnotWidget>("Knot");
--- src/plugin.hpp
+++ src/plugin.hpp
@@ -486,3 +486,35 @@ struct KnotUndoState
 		this->output4Index = output4Index;
 	}
 };
+
+
+enum KnotControl{
+	ShiftDown,
+	ShiftUp,
+	Reset,
+	Swap12,
+	Swap34,
+	Swap23,
+	Swap14,
+	Swap13,
+	Swap24,
+	Swap1234,
+	Swap1423,
+	Swap1324,
+	Random,
+	Undo,
+	Redo,
+};
+
+struct KnotPattern
+{
+	KnotControl pattern[64];
+	int patternLength = 0;
+	int patternIndex = 0;
+
+	KnotPattern(std::string patternString);
+	KnotControl getNextControl();
+	void reset();
+private:
+	KnotControl charToControl(char c);
+};
